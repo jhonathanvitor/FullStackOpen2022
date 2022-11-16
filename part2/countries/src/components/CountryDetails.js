c83a6ca864110e17f0327dd065af012ae9edcf84
jhonathan vitor <jhonathanvitor@gmail.com>
2022-11-16 14:22:36 -0400

    icons weather

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
+import axios from  'axios';
+
 const CountryDetails = ({country}) => {
+
+  const [temperature, setTemperature] = useState(0)
+  const [wind, setWind] = useState(0)
+  const [icon, setIcon] = useState('')
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&units=metric&appid=${process.env.REACT_APP_API_KEY}`).then((response) => {
+      setTemperature(response.data.main.temp)
+      setWind(response.data.wind.speed)
+      setIcon(response.data.weather[0].icon)
+  })
+
+
   return (
     <>
           <h1>{country.name}</h1>
-          <div>capital: {country.capital}</div>
+          <div>capital: {country.capital[0]}</div>
           <div>area: {country.area}</div>
           <h2>languages</h2>
           <ul>
@@ -11,6 +27,10 @@ const CountryDetails = ({country}) => {
           <div>
             <img src={country.flags.png} alt={`${country.name} flag`} />
           </div>
+          <h2>Weather in {country.capital[0]}</h2>
+          <div>temperature {temperature}º Celsius</div>
+          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+          <div>wind {wind} m/s</div>
         </>
   )
 }
